ab935811986c9ea7d82426f064c59bf38ec26708
Ricardo <ricardosaca@gmail.com>
2022-02-28 20:51:22 -0600

    end of day feb 28, base functionality done, need to calculate portfolio gain/loss

--- app.py
+++ app.py
@@ -1,10 +1,29 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from functions import get_bitcoin_price
+import datetime as dt
+from functions import dict_to_df, get_bitcoin_price, get_investment_value, get_latest_bitcoin_price, format_df
 from tweets import tweets
 
 st.title("Inversion del Gobierno de El Salvado en Bitcoin")
 
-for key in tweets:
-    tweets[key]
+
+bitPrice = get_latest_bitcoin_price('BTC-USD')
+
+f'The current price of Bitcoin is ${bitPrice[1]:,.2f} extracted at {bitPrice[0].strftime("%Y-%b-%d %H:%M %Z")}'
+
+tweetsData = get_bitcoin_price(tweets)
+tweetsData = get_investment_value(tweetsData)
+
+# tweetsDf = dict_to_df(tweetsData)
+tweetsDf = format_df(dict_to_df(tweetsData))
+
+pd.set_option('display.max_colwidth', -1)
+
+st.dataframe(tweetsDf)
+
+tweetsDf = tweetsDf.to_html(escape=True)
+
+st.write(tweetsDf, unsafe_allow_html=True)
+
+
--- functions.py
+++ functions.py
@@ -1,41 +1,80 @@
-from xml.dom import minidom
 import pandas as pd
 import numpy as np
 import yfinance as yf
 import datetime as dt
 from tweets import tweets
 
-def set_ticker(ticker):
-    stock = yf.Ticker(ticker)
-    return stock
+def get_latest_bitcoin_price(ticker):
+    df =yf.download(ticker, period='1d', interval='1m')
+    bitcoinPrice = df['Close'].iloc[-1]
+    time = dt.datetime.fromtimestamp(int(round(df.index[-1].timestamp())))
+
+    return [time, bitcoinPrice]
 
 def get_bitcoin_data(ticker, minDate, maxDate, column):
     df = yf.download(ticker, start=minDate, end=maxDate, interval="1h")
     return df[column]
 
-def get_bitcoin_price(tweet):
-    date = tweet['date']
-    startDate = date + dt.timedelta(hours=-1)
-    endDate = date + dt.timedelta(hours=1)
-    df = yf.download(tickers='BTC-USD', start=startDate, end=endDate, interval='1m')
-    print(df)
+def get_bitcoin_price(tweets):
+    #Set min and max date from tweets dictionary
+    minDate = tweets[min(tweets, key=lambda x:tweets[x]['date'])]['date'].replace(second=0, hour=0, minute=0)
+    maxDate = tweets[max(tweets, key=lambda x:tweets[x]['date'])]['date'].replace(second=0, hour=0, minute=0) + dt.timedelta(days=1)
 
-def main():
-
-    minDate = tweets[min(tweets, key=lambda x:tweets[x]['date'])]['date']
-    maxDate = tweets[max(tweets, key=lambda x:tweets[x]['date'])]['date']
-
+    #Retrieve bitcoin close data by the hour
     df = get_bitcoin_data("BTC-USD", minDate, maxDate, "Close")
-    print(type(pd.Timestamp(tweets[1]['date'])), type(df.index[1]))
-    print(df.index)
-    price =[]
+
+    #Identify price of tweet
     for tweet in tweets:
         date = tweets[tweet]['date'].strftime('%Y-%m-%d %H:00:00+00:00')
         date = pd.Timestamp(date)
-        print(type(date))
-        # print(date, df.loc(date))
-        print(df['2021-09-07 17:00:00+00:00'])
-        # price.append([date, df[date]])
+        bitPrice = df.iloc[df.index.get_indexer([date], method='nearest')][0]
+        tweets[tweet]['bitcoin_price'] = bitPrice
+
+    #Return tweets
+    return tweets
+
+def get_investment_value(tweets):
+
+    currentPrice = get_latest_bitcoin_price("BTC-USD")[1]
+    for tweet in tweets:
+        purchasedPrice = tweets[tweet]['bitcoin_price']
+        numCoins = tweets[tweet]['num_coins']
+        tweets[tweet]['original_cost'], tweets[tweet]['current_investment'], tweets[tweet]['gain/loss'], tweets[tweet]['pct gain/loss'] = get_gain_loss(currentPrice, purchasedPrice, numCoins)
+
+    return tweets
+
+def get_gain_loss(currentPrice, purchasedPrice, numCoins):
+    originalCost = numCoins * purchasedPrice
+    currentInvestment = numCoins * currentPrice
+    gainLoss = currentInvestment - originalCost
+    pctGainLoss = ((currentInvestment - originalCost)/originalCost) * 100
+
+    return originalCost, currentInvestment, gainLoss, pctGainLoss
 
 
-main()
+
+def dict_to_df(tweets):
+    tweetsDf = pd.DataFrame.from_dict(tweets, orient='index')
+    return tweetsDf
+
+def format_df(df):
+    df.insert(0, 'Tweet', df.apply( lambda row: f'<a target="_blank" href="{row.link}">{row.date.strftime("%Y-%b-%d %H:%M %Z")}</a>', axis=1 ))
+    df.drop(['link', 'date'], axis=1, inplace=True)
+    df.rename(columns={"num_coins":"Number of Coins", "bitcoin_price":"Bitcoin Price", "original_cost":"Investment Cost","gain/loss":"Gain/Loss","pct gain/loss":"% Gain/Loss"}, inplace=True)
+    return df
+
+if __name__ == "__main__":
+
+    tweetsData = get_bitcoin_price(tweets)
+    tweetsData = get_investment_value(tweetsData)
+
+    tweetsDf = format_df(dict_to_df(tweetsData))
+    print(tweetsDf)
+    #Calculate gain/loss return from investment
+    bitPrice = get_latest_bitcoin_price('BTC-USD')
+
+
+#To-Do:
+# * Gain/Loss of Tweet
+# * TZ for latest price retrieved
+# * Add disclaimer of delay in latest price & not included transaction costs
